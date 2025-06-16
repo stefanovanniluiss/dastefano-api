468cd86622bc59edb59faadbc3534e52c9df649e
stefanovanniluiss <stefanovanni@gmail.com>
2025-06-15 23:24:30 -0400

    Delete api/webhook.ts

--- api/webhook.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async (req: VercelRequest, res: VercelResponse) => {
-  if (req.method !== 'POST') return res.status(405).end();
-  console.log('Webhook:', req.body);
-  // TODO: guardar en DB / enviar mail
-  return res.json({ received: true });
-};
